3ed292cb17a4968bd98a6fbe3b0a0cd4c9e0ed3c
Deepak M <deepak.mishra@iqvia.com>
2021-12-23 19:54:38 +0530

    location query changed

--- server/src/controller/LocationController.js
+++ server/src/controller/LocationController.js
@@ -6,14 +6,14 @@ exports.searchLocationList = function (req, res) {
   try {
     const searchParam = req.params.query.toLowerCase();
     const searchQuery = `SELECT loc_id,loc_typ,ip_servr,port,usr_nm,pswd,cnn_url,data_strc,active,extrnl_sys_nm,loc_alias_nm from cdascdi1d.cdascdi.location 
-            WHERE LOWER(loc_typ) LIKE '%${searchParam}%' OR 
-            LOWER(loc_alias_nm) LIKE '%${searchParam}%'
+            WHERE LOWER(loc_typ) LIKE $1 OR 
+            LOWER(loc_alias_nm) LIKE $2
             `;
     Logger.info({
       message: "locationList",
     });
 
-    DB.executeQuery(searchQuery).then((response) => {
+    DB.executeQuery(searchQuery, [`%${searchParam}%`, `%${searchParam}%`]).then((response) => {
       const locations = response.rows || [];
       return apiResponse.successResponseWithData(res, "Operation success", {
         records: locations,
@@ -34,23 +34,27 @@ exports.getLocationList = function (req, res) {
     let type = req.query.type || null;
     let select = `loc_id,loc_typ,ip_servr,port,usr_nm,pswd,cnn_url,data_strc,active,extrnl_sys_nm,loc_alias_nm`;
     let searchQuery = `SELECT ${select} from cdascdi1d.cdascdi.location`;
+    let dbQuery = DB.executeQuery(searchQuery);
     if(type) {
         switch(type) {
             case 'rdbms_only':
                 searchQuery = `SELECT ${select} from cdascdi1d.cdascdi.location where loc_typ NOT IN('SFTP','FTPS')`;
+                dbQuery = DB.executeQuery(searchQuery);
             break;
             case 'ftp_only':
                 searchQuery = `SELECT ${select} from cdascdi1d.cdascdi.location where loc_typ IN('SFTP','FTPS')`;
+                dbQuery = DB.executeQuery(searchQuery);
             break;
             default:
-                searchQuery = `SELECT ${select} from cdascdi1d.cdascdi.location where loc_typ = '${type}'`;
+                searchQuery = `SELECT ${select} from cdascdi1d.cdascdi.location where loc_typ = $1`;
+                dbQuery = DB.executeQuery(searchQuery, [type]);
         }
     }
     Logger.info({
       message: "locationList",
     });
 
-    DB.executeQuery(searchQuery).then((response) => {
+    dbQuery.then((response) => {
       const locations = response.rows || [];
       return apiResponse.successResponseWithData(res, "Operation success", {
         records: locations,
@@ -70,12 +74,12 @@ exports.getLocationById = function (req, res) {
   try {
     const id = req.params.location_id;
     const searchQuery = `SELECT loc_id,loc_typ,ip_servr,port,usr_nm,pswd,cnn_url,data_strc,active,extrnl_sys_nm,loc_alias_nm from cdascdi1d.cdascdi.location 
-            WHERE loc_id = '${id}'`;
+            WHERE loc_id = $1`;
     Logger.info({
       message: "locationList",
     });
 
-    DB.executeQuery(searchQuery).then((response) => {
+    DB.executeQuery(searchQuery, [id]).then((response) => {
       const locations = response.rows[0] || null;
       return apiResponse.successResponseWithData(res, "Operation success", locations);
     });
