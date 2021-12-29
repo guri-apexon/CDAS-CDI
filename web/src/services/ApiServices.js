2873ce5058aa6a9c81b930c30d24a5c3a2a9f4f7
u1105372 <vaitheeswaran.p@iqvia.com>
2021-12-29 18:42:36 +0530

    update

--- web/src/services/ApiServices.js
+++ web/src/services/ApiServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { baseURL, STUDYSEARCH } from "../constants";
-import { getCookie } from "../utils/index";
+// import { getCookie } from "../utils/index";
 
 const searchStudy = async (searchQuery = "") => {
   try {
