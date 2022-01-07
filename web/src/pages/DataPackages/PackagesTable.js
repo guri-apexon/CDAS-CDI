b05b209d96542f525acaa19c37400ca23908ad9f
Deepak M <deepak.mishra@iqvia.com>
2022-01-07 21:03:11 +0530

    left side fixed

--- web/src/pages/DataPackages/PackagesTable.js
+++ web/src/pages/DataPackages/PackagesTable.js
@@ -1,88 +1,32 @@
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from "react";
-import Typography from "apollo-react/components/Typography";
+import { useDispatch } from "react-redux";
 import Tooltip from "apollo-react/components/Tooltip";
 import IconButton from "apollo-react/components/IconButton";
+import TreeItem from "apollo-react/components/TreeItem";
+import TreeView from "apollo-react/components/TreeView";
 import ArrowDown from "apollo-react-icons/ArrowDown";
 import ArrowRight from "apollo-react-icons/ArrowRight";
-import Table from "apollo-react/components/Table";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import Typography from "apollo-react/components/Typography";
 import Tag from "apollo-react/components/Tag";
 import EllipsisVertical from "apollo-react-icons/EllipsisVertical";
 import IconMenuButton from "apollo-react/components/IconMenuButton";
-import Menu from "apollo-react/components/Menu";
-import MenuItem from "apollo-react/components/MenuItem";
-import { useDispatch } from "react-redux";
+import { ReactComponent as DataPackageIcon } from "../../components/Icons/datapackage.svg";
 import {
   deletePackage,
   updateStatus,
 } from "../../store/actions/DataPackageAction";
 
-const ExpandCell = ({ row: { packageName, handleToggleRow, expanded } }) => {
-  return (
-    <div style={{ width: 12 }}>
-      <Tooltip title={expanded ? "Collapse" : "Expand"} disableFocusListener>
-        <IconButton
-          id="expand"
-          size="small"
-          // onClick={() => handleToggleRow(packageName)}
-        >
-          {expanded ? <ArrowDown /> : <ArrowRight />}
-        </IconButton>
-      </Tooltip>
-    </div>
-  );
-};
-const PackageImg = (
-  <img
-    src="assets/svg/datapackage.svg"
-    alt="datapackage"
-    style={{ width: 15, marginRight: 8 }}
-  />
-);
-const NameCustomCell = ({ row, column: { accessor } }) => {
-  const title = row[accessor] || row.datapackageid;
-  return (
-    <div className="flex package-name-td">
-      {PackageImg}
-      <span className="b-font">{title}</span>
-    </div>
-  );
-};
-const StatusCustomCell = ({ row, column: { accessor } }) => {
-  const active = Number(row[accessor]);
-  return (
-    <div className="flex">
-      {active === 1 ? (
-        <Tag label="Active" variant="green" />
-      ) : (
-        <Tag label="Inactive" variant="grey" />
-      )}
-    </div>
-  );
-};
-
-const DetailRow = ({ row }) => {
-  return (
-    <div style={{ display: "flex", padding: "8px 0px 8px 8px" }}>
-      <Typography>DataSets</Typography>
-    </div>
-  );
-};
-
 const PackagesList = ({ data, userInfo }) => {
   const dispatch = useDispatch();
   const [expandedRows, setExpandedRows] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
   const [tableData, setTableData] = useState([]);
-  const ActionCell = ({ row }) => {
-    const { packageName, onRowEdit } = row;
-    const active = row.active && Number(row.active) === 1 ? 1 : 0;
-    const packageId = row.datapackageid || null;
-    const [anchorEl, setAnchorEl] = useState(null);
-    const [open, setOpen] = useState(false);
-
-    const handleRequestClose = () => {
-      setOpen(false);
-    };
-    const setActive = (status) => {
+  const ContextMenu = (props) => {
+    const setActive = (packageId, status) => {
       if (packageId) {
         dispatch(
           updateStatus({
@@ -93,7 +37,7 @@ const PackagesList = ({ data, userInfo }) => {
         );
       }
     };
-    const deleteAction = () => {
+    const deleteAction = (packageId) => {
       if (packageId) {
         dispatch(
           deletePackage({ package_id: packageId, user_id: userInfo.user_id })
@@ -102,108 +46,120 @@ const PackagesList = ({ data, userInfo }) => {
     };
     const menuItems = [
       {
-        text: `Set data package ${active === 1 ? "inactive" : "active"}`,
-        onClick: () => setActive(active),
+        text: `Set data package ${props.active === 1 ? "inactive" : "active"}`,
+        onClick: () => setActive(props.datapackageid, props.active),
       },
       {
         text: "Set all dataset to active",
-        // onClick: () => onRowEdit(packageName),
+        // onClick: () => onRowEdit(props.datapackageid),
       },
       {
         text: "Set all datasets to inactive",
-        // onClick: () => onRowEdit(packageName),
+        // onClick: () => onRowEdit(props.datapackageid),
       },
       {
         text: "Delete data package",
-        onClick: deleteAction,
+        onClick: () => deleteAction(props.datapackageid),
       },
     ];
-    const openAction = (e) => {
-      setAnchorEl(e.currentTarget);
-      setOpen(true);
-    };
-
     return (
       <>
-        <EllipsisVertical fontSize="small" onClick={openAction} />
-        <Menu
-          id="tableMenu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleRequestClose}
-        >
-          {menuItems.map((menu) => {
-            return (
-              <MenuItem size="small" onClick={menu.onClick}>
-                {menu.text}
-              </MenuItem>
-            );
-          })}
-        </Menu>
+        <Tooltip title="Actions" disableFocusListener>
+          <IconMenuButton id="actions" menuItems={menuItems} size="small">
+            <EllipsisVertical />
+          </IconMenuButton>
+        </Tooltip>
       </>
-      // <IconMenuButton menuItems={menuItems} size="small">
-      //   <EllipsisVertical />
-      // </IconMenuButton>
     );
-  };
-  const columns = [
-    {
-      accessor: "expand",
-      customCell: ExpandCell,
-      width: 20,
-    },
-    {
-      header: "Package Name",
-      accessor: "name",
-      customCell: NameCustomCell,
-    },
-    {
-      header: "Active",
-      accessor: "active",
-      customCell: StatusCustomCell,
-      width: 80,
-    },
-    {
-      accessor: "action",
-      customCell: ActionCell,
-      align: "right",
-      width: 32,
-    },
-  ];
-  const handleToggleRow = (packageName) => {
-    // eslint-disable-next-line no-shadow
-    setExpandedRows((expandedRows) =>
-      expandedRows.includes(packageName)
-        ? expandedRows.filter((id) => id !== packageName)
-        : [...expandedRows, packageName]
-    );
-  };
+  }
+  const renderChildTree = ({ datasetsid, name }) => (
+    <TreeItem key={datasetsid} nodeId={datasetsid} label={name} />
+  );
+  const DataPackageTitle = (props) => (
+    <div>
+      <Typography variant="caption" className="datasetCount">0</Typography>
+      <Tooltip title="Add Dataset" placement="bottom">
+        <IconButton size="small">
+          <AddCircleIcon />
+        </IconButton>
+      </Tooltip>
+      <Tag
+        label={props.active ? "Active" : "Inactive"}
+        variant={props.active ? "green" : "grey"}
+      />
+      <ContextMenu {...props} />
+    </div>
+  );
+  const renderTree = ({ datapackageid, name, childNodes, active }) => (
+    <TreeItem
+      key={datapackageid}
+      nodeId={datapackageid}
+      label={name}
+      count={<DataPackageTitle active={active} datapackageid={datapackageid} name={name} />}
+      icon={
+        <>
+          {expandedRows.indexOf(datapackageid) === -1 && (
+            <ArrowRight
+              style={{
+                padding: "4px 4px 4px 0px",
+                fontSize: "16px !important",
+                paddingLeft: "5px !important",
+              }}
+            />
+          )}
+          {expandedRows.indexOf(datapackageid) > -1 && (
+            <ArrowDown
+              style={{
+                padding: "4px 4px 4px 0px",
+                fontSize: "16px !important",
+                paddingLeft: "5px !important",
+              }}
+            />
+          )}
+          <DataPackageIcon
+            style={{
+              marginTop: "1px",
+              marginLeft: "-4px",
+              marginRight: "-16px",
+              color: "#595959",
+            }}
+          />
+        </>
+      }
+      // eslint-disable-next-line react/no-children-prop
+      children={<DataPackageTitle />}
+    >
+      {Array.isArray(childNodes)
+        ? childNodes.map((node) => renderChildTree(node))
+        : null}
+    </TreeItem>
+  );
   useEffect(() => {
     const newData = data.packagesList || [];
-    console.log("Tabledata", newData);
     const updatedData = newData;
-    // tableData.map((row) => ({
-    //   ...row,
-    //   expanded: expandedRows.includes(row.packageName),
-    //   handleToggleRow,
-    // }));
     setTableData(updatedData);
   }, [data.packagesList]);
+  const handleToggle = (e, nodeIds) => {
+    setExpandedRows(nodeIds);
+  };
+  const handleSelect = (e, nodeIds) => {
+    setSelectedRows(nodeIds);
+  };
+  console.log(expandedRows, "expandedRows");
   return (
-    <Table
-      columns={columns}
-      rowId="packageName"
-      rows={tableData}
-      style={{ overflow: "hidden" }}
-      rowProps={{ hover: false }}
-      hidePagination={true}
-      // tablePaginationProps={{
-      //   labelDisplayedRows: ({ from, to, count }) =>
-      //     `${count === 1 ? "Package" : "Packages"} ${from}-${to} of ${count}`,
-      //   truncate: true,
-      // }}
-      ExpandableComponent={DetailRow}
-    />
+    <>
+      <div style={{ maxWidth: 400 }}>
+        <TreeView
+          expanded={expandedRows}
+          selected={selectedRows}
+          variant="folder"
+          onNodeToggle={handleToggle}
+          multiSelect
+        >
+          {tableData.map((node) => renderTree(node))}
+        </TreeView>
+      </div>
+    </>
   );
 };
 export default PackagesList;
