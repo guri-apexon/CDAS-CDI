32c8ad328f62cb19d51def06a703b1de3af1ccae
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-01-14 18:22:57 +0530

    Download added for audit log

--- web/src/pages/AuditLog/AuditLog.js
+++ web/src/pages/AuditLog/AuditLog.js
@@ -1,19 +1,26 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/no-array-index-key */
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import "./AuditLog.scss";
+import * as XLSX from "xlsx";
+import { pick } from "lodash";
+import moment from "moment";
 import Paper from "apollo-react/components/Paper";
 import Typography from "apollo-react/components/Typography";
 import BreadcrumbsUI from "apollo-react/components/Breadcrumbs";
 import Box from "apollo-react/components/Box";
 import Table from "apollo-react/components/Table";
+import Button from "apollo-react/components/Button";
+import DownloadIcon from "apollo-react-icons/Download";
+import FilterIcon from "apollo-react-icons/Filter";
 import ButtonGroup from "apollo-react/components/ButtonGroup";
 import PageHeader from "../../components/DataFlow/PageHeader";
 import columns from "./columns.data";
 // import { rowsWithExtra } from "./rows.data";
 import { getAuditLogs } from "../../store/actions/AuditLogsAction";
+import { MessageContext } from "../../components/MessageProvider";
 
 const breadcrumpItems = [
   { href: "/dashboard" },
@@ -26,15 +33,161 @@ const breadcrumpItems = [
   },
 ];
 const AuditLog = ({ match }) => {
+  const [rowsPerPageRecord, setRowPerPageRecord] = useState(10);
+  const [pageNo, setPageNo] = useState(0);
+  const auditLogs = useSelector((state) => state.auditLogs);
+  const auditData = auditLogs.data;
+  const [sortedColumnValue, setSortedColumnValue] = useState("dateadded");
+  const [sortOrderValue, setSortOrderValue] = useState("asc");
+  const [inlineFilters, setInlineFilters] = useState([]);
+
+  const [tableRows, setTableRows] = useState([...auditData]);
+  const [exportTableRows, setExportTableRows] = useState([...auditData]);
+  // const [tableColumns, setTableColumns] = useState([...moreColumns]);
   const dispatch = useDispatch();
   const { dataflowId } = useParams();
-  const auditLogs = useSelector((state) => state.auditLogs);
+  const messageContext = useContext(MessageContext);
   const fetchLogs = () => {
     dispatch(getAuditLogs(dataflowId));
   };
+
+  const exportToCSV = (exportData, headers, fileName) => {
+    // console.log("data for export", exportData, headers, fileName);
+    const wb = XLSX.utils.book_new();
+    let ws = XLSX.worksheet;
+    const from = pageNo * rowsPerPageRecord;
+    const to = from + rowsPerPageRecord;
+    const newData = exportData.slice(from, to);
+    newData.unshift(headers);
+    console.log("data", from, rowsPerPageRecord, newData);
+    ws = XLSX.utils.json_to_sheet(newData, { skipHeader: true });
+    XLSX.utils.book_append_sheet(wb, ws, "studylist");
+    XLSX.writeFile(wb, fileName);
+    exportData.shift();
+  };
+  const applyFilter = (cols, rows, filts) => {
+    let filteredRows = rows;
+    Object.values(cols).forEach((column) => {
+      if (column.filterFunction) {
+        filteredRows = filteredRows.filter((row) => {
+          return column.filterFunction(row, filts);
+        });
+        if (column.sortFunction) {
+          filteredRows.sort(
+            column.sortFunction(sortedColumnValue, sortOrderValue)
+          );
+        }
+      }
+    });
+    return filteredRows;
+  };
+
+  const exportDataRows = () => {
+    const toBeExportRows = [...auditData];
+    const sortedFilteredData = applyFilter(
+      columns,
+      toBeExportRows,
+      inlineFilters
+    );
+    setExportTableRows(sortedFilteredData);
+    return sortedFilteredData;
+  };
+
+  const downloadFileMethod = async (e) => {
+    const fileExtension = ".xlsx";
+    const fileName = `StudyList_${moment(new Date()).format("DDMMYYYY")}`;
+    // console.log("inDown", exportHeader);
+    const tempObj = {};
+    const temp = columns
+      .filter((d) => d.hidden !== true)
+      .map((d) => {
+        tempObj[d.accessor] = d.header;
+        return d;
+      });
+    const newData = exportTableRows.map((obj) => {
+      const newObj = pick(obj, Object.keys(tempObj));
+      return newObj;
+    });
+    exportToCSV(newData, tempObj, fileName + fileExtension);
+    const exportRows = exportDataRows();
+    if (exportRows.length <= 0) {
+      e.preventDefault();
+      const message = `There is no data on the screen to download because of which an empty file has been downloaded.`;
+      messageContext.showErrorMessage(message);
+    } else {
+      const message = `File downloaded successfully.`;
+      messageContext.showSuccessMessage(message);
+    }
+  };
+
+  const CustomButtonHeader = ({ toggleFilters, downloadFile }) => (
+    <div>
+      <Button
+        size="small"
+        variant="secondary"
+        icon={DownloadIcon}
+        onClick={downloadFile}
+        style={{ marginRight: "8px", border: "none" }}
+      >
+        Download
+      </Button>
+      <Button
+        size="small"
+        variant="secondary"
+        icon={FilterIcon}
+        onClick={toggleFilters}
+      >
+        Filter
+      </Button>
+    </div>
+  );
+
+  useEffect(() => {
+    setTableRows(auditData);
+  }, [auditData]);
   useEffect(() => {
     fetchLogs();
   }, []);
+
+  const getTableData = React.useMemo(() => (
+    <>
+      <Table
+        title="Data Flow Audit Log"
+        columns={columns}
+        rows={tableRows}
+        initialSortedColumn="name"
+        sortedColumn={sortedColumnValue}
+        sortOrder={sortOrderValue}
+        rowsPerPageOptions={[10, 50, 100, "All"]}
+        tablePaginationProps={{
+          labelDisplayedRows: ({ from, to, count }) =>
+            `${count === 1 ? "Item " : "Items"} ${from}-${to} of ${count}`,
+          truncate: true,
+        }}
+        page={pageNo}
+        rowsPerPage={rowsPerPageRecord}
+        onChange={(rpp, sc, so, filts, page) => {
+          // console.log("onChange", rpp, sc, so, filts, page, others);
+          setRowPerPageRecord(rpp);
+          setSortedColumnValue(sc);
+          setSortOrderValue(so);
+          setInlineFilters(filts);
+          setPageNo(page);
+        }}
+        columnSettings={{
+          enabled: true,
+          defaultColumns: columns,
+          // onChange: (changeColumns) => {
+          //   setTableColumns(changeColumns);
+          // },
+        }}
+        CustomHeader={(props) => (
+          <CustomButtonHeader downloadFile={downloadFileMethod} {...props} />
+        )}
+      />
+    </>
+  ));
+
   return (
     <main className="audit-logs-wrapper">
       <PageHeader height={60} />
@@ -70,7 +223,8 @@ const AuditLog = ({ match }) => {
         </Box>
       </Paper>
       <Box padding={3}>
-        <Table
+        {getTableData}
+        {/* <Table
           key="frozenExample1"
           title="Data Flow Audit Log"
           columns={columns}
@@ -89,7 +243,11 @@ const AuditLog = ({ match }) => {
             frozenColumnsEnabled: true,
             defaultColumns: columns,
           }}
-        />
+          CustomHeader={(props) => (
+            <CustomButtonHeader downloadFile={downloadFile} {...props} />
+          )}
+
+        /> */}
       </Box>
     </main>
   );
