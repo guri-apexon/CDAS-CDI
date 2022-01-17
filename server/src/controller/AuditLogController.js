d8331c9ec7161cd946cbcc3767a301ce365d3986
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-01-17 21:51:30 +0530

    changes

--- server/src/controller/AuditLogController.js
+++ server/src/controller/AuditLogController.js
@@ -7,9 +7,9 @@ const moment = require("moment");
 exports.searchList = function (req, res) {
   try {
     const dataflowId = req.params.dataflowId;
-    const searchQuery = `SELECT al.*, u.usr_fst_nm user_name, dp.name from cdascdi1d.cdascdi.dataflow_audit_log as al
+    const searchQuery = `SELECT al.*, CONCAT(al.audit_vers, '.0') as log_version, to_char(al.audit_updt_dt , 'DD-mon-YYYY HH:MI AM') as update_dt, u.usr_fst_nm user_name, dp.name from cdascdi1d.cdascdi.dataflow_audit_log as al
     LEFT JOIN cdascdi1d.cdascdi.datapackage as dp ON dp.datapackageid = al.datapackageid
-    LEFT JOIN cdascdi1d.cdascdi.user as u ON u.usr_id = al.usr_id
+    LEFT JOIN cdascdi1d.cdascdi.user as u ON u.usr_id = al.audit_updt_by
     WHERE al.dataflowid='${dataflowId}'`;
     Logger.info({
       message: "AuditLogs",
