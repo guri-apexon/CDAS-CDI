f5392176b3dce352b4174df2b29000f2104134f1
Deepak M <deepak.mishra@iqvia.com>
2022-01-18 17:12:29 +0530

    merge

--- web/src/pages/DataSets/DataSets.js
+++ web/src/pages/DataSets/DataSets.js
@@ -161,7 +161,7 @@ const DataSets = () => {
         />
       )}
       <div className={classes.toolbar} />
-      <Leftbar />
+      {/* <Leftbar /> */}
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <div className="content">
