4109a0be794e4f54e00705d417e54f831e210a07
u1105372 <vaitheeswaran.p@iqvia.com>
2022-01-21 12:06:10 +0530

    code clean up

--- web/src/pages/AuditLog/columns.data.js
+++ web/src/pages/AuditLog/columns.data.js
@@ -1,84 +1,13 @@
-import Bullseye from "apollo-react-icons/Bullseye";
-import Cog from "apollo-react-icons/Cog";
-import OpenNew from "apollo-react-icons/OpenNew";
-import Pencil from "apollo-react-icons/Pencil";
-import Question from "apollo-react-icons/Question";
-import Rocket from "apollo-react-icons/Rocket";
-import Services from "apollo-react-icons/Services";
-import User3 from "apollo-react-icons/User3";
 import moment from "moment";
 import React from "react";
-
-import AutocompleteV2 from "apollo-react/components/AutocompleteV2";
-import DateRangePickerV2 from "apollo-react/components/DateRangePickerV2";
-import IconButton from "apollo-react/components/IconButton";
 import {
   compareDates,
   compareNumbers,
   compareStrings,
-  createSelectFilterComponent,
   createStringSearchFilter,
   dateFilterV2,
-  numberSearchFilter,
 } from "apollo-react/components/Table";
-import { TextField } from "apollo-react/components/TextField/TextField";
-import { rows } from "./rows.data";
-
-const TextFieldFilter = ({ accessor, filters, updateFilterValue }) => {
-  return (
-    <TextField
-      value={filters[accessor]}
-      name={accessor}
-      onChange={updateFilterValue}
-      fullWidth
-      margin="none"
-      size="small"
-    />
-  );
-};
-
-export const IntegerFilter = ({ accessor, filters, updateFilterValue }) => {
-  return (
-    <TextField
-      value={filters[accessor]}
-      name={accessor}
-      onChange={updateFilterValue}
-      type="number"
-      style={{ width: 74 }}
-      margin="none"
-      size="small"
-    />
-  );
-};
-
-const DateFilter = ({ accessor, filters, updateFilterValue }) => {
-  return (
-    <div style={{ minWidth: 230 }}>
-      <div style={{ position: "absolute", top: 0, paddingRight: 4 }}>
-        <DateRangePickerV2
-          value={filters[accessor] || [null, null]}
-          name={accessor}
-          onChange={(value) =>
-            updateFilterValue({
-              target: { name: accessor, value },
-            })
-          }
-          startLabel=""
-          endLabel=""
-          placeholder=""
-          fullWidth
-          margin="none"
-          size="small"
-        />
-      </div>
-    </div>
-  );
-};
-
-const VersionCell = ({ row, column: { accessor } }) => {
-  const version = row[accessor];
-  return <span>{`${version}.0`}</span>;
-};
+import { TextFieldFilter, DateFilter } from "../../utils/index";
 
 const DateCell = ({ row, column: { accessor } }) => {
   const rowValue = row[accessor];
@@ -89,72 +18,6 @@ const DateCell = ({ row, column: { accessor } }) => {
 
   return <span>{date}</span>;
 };
-
-const createAutocompleteFilter =
-  (source) =>
-  ({ accessor, filters, updateFilterValue }) => {
-    const ref = React.useRef();
-    const [height, setHeight] = React.useState(0);
-    const [isFocused, setIsFocused] = React.useState(false);
-    const value = filters[accessor];
-
-    React.useEffect(() => {
-      const curHeight = ref?.current?.getBoundingClientRect().height;
-      if (curHeight !== height) {
-        setHeight(curHeight);
-      }
-    }, [value, isFocused, height]);
-
-    return (
-      <div
-        style={{
-          minWidth: 144,
-          maxWidth: 200,
-          position: "relative",
-          height,
-        }}
-      >
-        <AutocompleteV2
-          style={{ position: "absolute", left: 0, right: 0 }}
-          value={value ? value.map((label) => ({ label })) : []}
-          name={accessor}
-          source={source}
-          onChange={(event, v) =>
-            updateFilterValue({
-              target: {
-                name: accessor,
-                value: v.map(({ label }) => label),
-              },
-            })
-          }
-          fullWidth
-          multiple
-          chipColor="white"
-          size="small"
-          forcePopupIcon
-          showCheckboxes
-          limitChips={1}
-          filterSelectedOptions={false}
-          blurOnSelect={false}
-          clearOnBlur={false}
-          disableCloseOnSelect
-          showSelectAll
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          ref={ref}
-        />
-      </div>
-    );
-  };
-
-export function createStringArraySearchFilter(accessor) {
-  return (row, filters) =>
-    !Array.isArray(filters[accessor]) ||
-    filters[accessor].length === 0 ||
-    filters[accessor].some(
-      (value) => value.toUpperCase() === row[accessor]?.toUpperCase()
-    );
-}
 
 const columns = [
   {
