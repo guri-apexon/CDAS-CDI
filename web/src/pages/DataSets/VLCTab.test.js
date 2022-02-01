4c5dd71f16f91e6ca8416d0ac09c46c2af13fcd4
u1105372 <vaitheeswaran.p@iqvia.com>
2022-02-01 15:47:30 +0530

    update

--- web/src/pages/DataSets/VLCTab.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { mount } from "enzyme";
-import VLCTab from "./VLCTab";
-
-test("VLCTab Table component renders count", () => {
-  const wrapper = mount(<VLCTab />);
-  expect(wrapper.exists(<table />)).toBe(true);
-});
