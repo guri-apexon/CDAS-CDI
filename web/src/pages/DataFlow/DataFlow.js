f7faab88d79f402f6d9f865b6fb077b74e562567
u1105372 <vaitheeswaran.p@iqvia.com>
2022-02-01 13:58:18 +0530

    not allow if dataflow is selected

--- web/src/pages/DataFlow/DataFlow.js
+++ web/src/pages/DataFlow/DataFlow.js
@@ -123,6 +123,12 @@ const DataFlow = ({ FormValues, dashboard }) => {
     history.push("/dashboard");
   };
 
+  useEffect(() => {
+    if (!dashboard?.selectedCard?.prot_id) {
+      history.push("/dashboard");
+    }
+  }, [dashboard?.selectedCard]);
+
   const submitForm = async () => {
     const protId = dashboard.selectedCard.prot_id;
     console.log("FormValues?", FormValues);
