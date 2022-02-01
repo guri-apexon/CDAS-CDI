9e78404a2baf0bb8c1ee880a0636817626781418
u1105372 <vaitheeswaran.p@iqvia.com>
2022-02-01 18:09:56 +0530

    update of study controller

--- server/src/controller/StudyController.js
+++ server/src/controller/StudyController.js
@@ -8,7 +8,7 @@ const constants = require("../config/constants");
 exports.getUserStudyList = function (req, res) {
   try {
     const userId = req.params.userId;
-    const query = `SELECT s.prot_id, prot_nbr as protocolnumber, spnsr_nm as sponsorname, phase, prot_stat as protocolstatus, proj_cd as projectcode FROM ${constants.DB_SCHEMA_NAME}.study s INNER JOIN ${constants.DB_SCHEMA_NAME}.sponsor s2 ON s2.spnsr_id = s.spnsr_id INNER JOIN ${constants.DB_SCHEMA_NAME}.study_user s3 ON s.prot_id = s3.prot_id  WHERE s3.usr_id = $1 ORDER BY sponsorname`;
+    const query = `SELECT distinct s.prot_id, prot_nbr as protocolnumber, spnsr_nm as sponsorname, phase, prot_stat as protocolstatus, proj_cd as projectcode FROM ${constants.DB_SCHEMA_NAME}.study s INNER JOIN ${constants.DB_SCHEMA_NAME}.sponsor s2 ON s2.spnsr_id = s.spnsr_id INNER JOIN ${constants.DB_SCHEMA_NAME}.study_user s3 ON s.prot_id = s3.prot_id  WHERE s3.usr_id = $1 ORDER BY sponsorname Limit 10`;
     // const q2 = `select dataflowid, COUNT(DISTINCT datapackageid) FROM ${constants.DB_SCHEMA_NAME}.datapackage d GROUP BY d.dataflowid`
     // const q3 = `select datapackageid, COUNT(DISTINCT datasetid) FROM ${constants.DB_SCHEMA_NAME}.dataset d GROUP BY datapackageid`
 
