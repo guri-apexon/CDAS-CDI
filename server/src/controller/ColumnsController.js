5ef667ace83cebcee8d802446925f1562c02b5f0
u1105372 <vaitheeswaran.p@iqvia.com>
2022-02-11 00:49:50 +0530

    update

--- server/src/controller/ColumnsController.js
+++ server/src/controller/ColumnsController.js
@@ -9,7 +9,7 @@ exports.getColumnsSet = async (req, res) => {
   try {
     const { datasetid } = req.body;
     Logger.info({ message: "getColumnsSet" });
-    const searchQuery = `SELECT "columnid", "VARIABLE", "name", "datatype", "primarykey", "required", "charactermin", "charactermax", "position", "FORMAT", "lov", "UNIQUE" from ${schemaName}.columndefinition WHERE datasetid = $1`;
+    const searchQuery = `SELECT "columnid", variable, "name", "datatype", "primarykey", "required", "charactermin", "charactermax", "position", format, "lov", "unique" from ${schemaName}.columndefinition WHERE datasetid = $1`;
     DB.executeQuery(searchQuery, [datasetid]).then((response) => {
       const datasetColumns = response.rows || null;
       return apiResponse.successResponseWithData(
