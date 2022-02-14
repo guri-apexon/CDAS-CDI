0244962fa38369d1a462f807b03760f51ba04d5f
u1105372 <vaitheeswaran.p@iqvia.com>
2022-02-14 12:05:24 +0530

    update on dataset insert and update

--- server/src/controller/DatasetController.js
+++ server/src/controller/DatasetController.js
@@ -67,7 +67,7 @@ exports.saveDatasetData = async (req, res) => {
     }
     const body = [
       datasetId,
-      values.datasetName || null,
+      values.datasetName,
       values.fileType || null,
       values.encoding || null,
       values.delimiter || null,
@@ -86,7 +86,7 @@ exports.saveDatasetData = async (req, res) => {
       new Date(),
       values.datapackageid,
     ];
-    const searchQuery = `INSERT into ${schemaName}.dataset (datasetid, mnemonic, type, charset, delimitier, escapecode, quote, headerrownumber, footerrownumber, active, naming_convention, path, datakindid, data_freq, ovrd_stale_alert, rowdecreaseallowed, insrt_tm, updt_tm, datapackageid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`;
+    const searchQuery = `INSERT into ${schemaName}.dataset (datasetid, mnemonic, type, charset, delimiter, escapecode, quote, headerrownumber, footerrownumber, active, naming_convention, path, datakindid, data_freq, ovrd_stale_alert, rowdecreaseallowed, insrt_tm, updt_tm, datapackageid) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`;
 
     await DB.executeQuery(searchQuery, body);
   } catch (err) {
@@ -124,7 +124,7 @@ exports.updateDatasetData = async (req, res) => {
       values.rowDecreaseAllowed || 0,
       new Date(),
     ];
-    const searchQuery = `UPDATE ${schemaName}.dataset set mnemonic = $1, type = $2, charset = $3, delimitier = $4, escapecode = $5, quote = $6, headerrownumber = $7, footerrownumber = $8, active = $9, naming_convention = $10, path = $11, datakindid = $12, data_freq = $13, ovrd_stale_alert = $14, rowdecreaseallowed = $15, updt_tm = $16 where datasetid = $17`;
+    const searchQuery = `UPDATE ${schemaName}.dataset set mnemonic = $1, type = $2, charset = $3, delimiter = $4, escapecode = $5, quote = $6, headerrownumber = $7, footerrownumber = $8, active = $9, naming_convention = $10, path = $11, datakindid = $12, data_freq = $13, ovrd_stale_alert = $14, rowdecreaseallowed = $15, updt_tm = $16 where datasetid = $17`;
 
     DB.executeQuery(searchQuery, [...body, values.datasetid]).then(() => {
       const hisBody = [
