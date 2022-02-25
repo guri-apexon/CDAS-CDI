27689bd948dd90eed14425a8df15f1706f3f8ef5
Deepak M <deepak.mishra@iqvia.com>
2022-02-25 16:37:27 +0530

    wip

--- web/src/pages/Admin/Settings/SystemSettings.js
+++ web/src/pages/Admin/Settings/SystemSettings.js
@@ -25,11 +25,18 @@ import {
 
 import "./SystemSettings.scss";
 
-const CustomHeader = ({ addSingleRow, disableCreateMode }) => (
+const CustomHeader = ({
+  addSingleRow,
+  disableCreateMode,
+  onSearch,
+  search,
+}) => (
   <div style={{ display: "flex", alignItems: "center" }}>
     <Search
       placeholder="Search"
       id="settingSearch"
+      value={search}
+      onChange={(e) => onSearch(e.target.value)}
       style={{ marginRight: 8, width: 240, marginTop: 6 }}
       size="small"
     />
@@ -138,6 +145,7 @@ const SystemSettings = () => {
     useSelector((state) => state.cdiadmin);
   const [totalsettings, setTotalSettings] = useState(0);
   const [rows, setRows] = useState([]);
+  const [search, setSearch] = useState("");
   const [editedRow, setEditedRow] = useState({});
   const [disableCreateMode, setDisableCreateMode] = useState(false);
 
@@ -154,6 +162,10 @@ const SystemSettings = () => {
     setRows(settings?.records ?? []);
     setTotalSettings(settings.totalSize ?? 0);
   }, [settings]);
+
+  const onSearch = (value) => {
+    setSearch(value);
+  };
 
   const addSingleRow = () => {
     const r = [
@@ -203,6 +215,14 @@ const SystemSettings = () => {
     }
   }, [error, success]);
 
+  // console.log("testtes");
+
+  const filteredRow = search
+    ? rows?.filter((row) =>
+        row.name?.toLowerCase().includes(search.toLowerCase())
+      )
+    : rows;
+
   return (
     <div className="system-table">
       {upsertLoading && <Loader />}
@@ -210,9 +230,9 @@ const SystemSettings = () => {
         title="System Settings"
         isLoading={loading}
         className="system-table"
-        subtitle={`${totalsettings} settings`}
+        subtitle={`${filteredRow.length} settings`}
         columns={columns}
-        rows={rows.map((row) => ({
+        rows={filteredRow.map((row) => ({
           ...row,
           onRowEdit,
           editedRow,
@@ -223,15 +243,16 @@ const SystemSettings = () => {
         }))}
         rowId="config_id"
         rowProps={{ hover: false }}
-        CustomHeader={(props) => (
+        CustomHeader={() => (
           <CustomHeader
             addSingleRow={addSingleRow}
+            onSearch={(v) => onSearch(v)}
+            search={search}
             disableCreateMode={disableCreateMode}
-            {...props}
           />
         )}
         // hidePagination={true}
-        rowsPerPageOptions={[10, 50, 100, "All"]}
+        rowsPerPageOptions={[50, 100, "All"]}
         tablePaginationProps={{
           labelDisplayedRows: ({ from, to, count }) =>
             `${count === 1 ? "Item" : "Items"} ${from}-${to} of ${count}`,
