8d7839790c244301a645c31e7d0370ac275a2d6c
u1120581 <nasim.alam@iqvia.com>
2022-03-07 17:08:40 +0530

    Hard Delete 7530

--- server/src/route/hardDelete.js
+++ server/src/route/hardDelete.js
@@ -2,6 +2,6 @@ var express = require("express");
 const HardDeleteController = require("../controller/HardDeleteController");
 var router = express.Router();
 
-router.post("/add-edit-delete", HardDeleteController.addEdit);
+router.post("/hard-delete", HardDeleteController.hardDelete);
 
 module.exports = router;
