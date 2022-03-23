484f23089628041b65698c2afffc8db759b06632
u1105372 <vaitheeswaran.p@iqvia.com>
2022-03-23 10:40:03 +0530

    completion of 10018

--- server/src/controller/ColumnsController.js
+++ server/src/controller/ColumnsController.js
@@ -32,32 +32,32 @@ exports.saveDatasetColumns = async (req, res) => {
   try {
     const datasetid = req.params.datasetid;
     const values = req.body;
-    const insertQuery = `INSERT into ${schemaName}.columndefinition (columnid, "VARIABLE", datasetid, name, datatype, primarykey, required, "UNIQUE", charactermin, charactermax, position, "FORMAT", lov, insrt_tm, updt_tm) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`;
+    const insertQuery = `INSERT into ${schemaName}.columndefinition (columnid, "VARIABLE", datasetid, name, position, datatype, primarykey, required, "UNIQUE", charactermin, charactermax, "FORMAT", lov, insrt_tm, updt_tm) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`;
+    Logger.info({ message: "storeDatasetColumns" });
+
     const inserted = await values.map(async (value) => {
       const columnId = helper.generateUniqueID();
-      Logger.info({
-        message: "storeDatasetColumns",
-      });
       const body = [
         columnId,
         value.variableLabel.trim() || null,
         datasetid,
         value.columnName.trim() || null,
+        value.position.trim() || null,
         value.dataType.trim() || null,
         value.primary == "Yes" ? 1 : 0,
         value.required == "Yes" ? 1 : 0,
         value.unique == "Yes" ? 1 : 0,
         value.minLength.trim() || null,
         value.maxLength.trim() || null,
-        value.position.trim() || null,
         value.format.trim() || null,
         value.values.trim().replace(/(^\~+|\~+$)/, "") || null,
-        new Date(),
-        new Date(),
+        helper.getCurrentTime(),
+        helper.getCurrentTime(),
       ];
-      const inserted = await DB.executeQuery(insertQuery, body);
-      return inserted;
+      const insrted = await DB.executeQuery(insertQuery, body);
+      return insrted;
     });
+
     Promise.all(inserted).then((response) => {
       if (response[0] == "SUCCESS") {
         return apiResponse.successResponseWithData(
