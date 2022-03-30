07be1acf843ed5859faafed4e2bf700b35361fbe
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-03-30 17:28:37 +0530

    changes dataflow datset count

--- web/src/pages/DataFlow/ViewEdit/LeftPanel/index.js
+++ web/src/pages/DataFlow/ViewEdit/LeftPanel/index.js
@@ -207,7 +207,7 @@ const LeftPanel = ({ dataflowId, headerTitle, dataflowSource }) => {
               <>
                 <Typography variant="body2" style={{ marginLeft: 10 }}>
                   {packagesList.length}
-                  {packagesList.length > 1 ? "Data Packages" : "Data Package"}
+                  {packagesList.length > 1 ? " Data Packages" : " Data Package"}
                 </Typography>
                 <PackagesList userInfo={userInfo} data={packageData} />
               </>
