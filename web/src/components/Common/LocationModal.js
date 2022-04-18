b46ced610c21be22b9bce452c4c895d8b18bad5f
Shaikh <junaid.shaikh@iqvia.com>
2022-04-18 23:23:17 +0530

    Location Edit-Username and password are Visible, removed unused data

--- web/src/components/Common/LocationModal.js
+++ web/src/components/Common/LocationModal.js
@@ -465,7 +465,6 @@ const ReduxForm = compose(
     selectedPort: selector(state, "port"),
     selectedDB: selector(state, "dbName"),
     locType: selector(state, "locationType"),
-    jhui: selector(state, "username"),
     formState: getFormValues("AddLocationForm")(state),
   }))
 )(LocationForm);
