dbcf7520c3c20c87d79f384ebd8f16d48cfce356
u1105372 <vaitheeswaran.p@iqvia.com>
2022-04-19 00:47:21 +0530

    updates for CDAS-10650

--- web/src/pages/Dataset/ColumnsTab/DSColumnTable.js
+++ web/src/pages/Dataset/ColumnsTab/DSColumnTable.js
@@ -174,44 +174,6 @@ export default function DSColumnTable({
     setSelectedRow(row);
   };
 
-  const handleSaveLOV = async () => {
-    if (selectedRow.dbColumnId) {
-      const newQuery = "";
-      const removeExistingRowData = rows.filter(
-        (e) => e.uniqueId !== selectedRow.uniqueId
-      );
-      const newData = [{ ...selectedRow }]
-        .map((e) => {
-          e.values = e.values.trim();
-          return e;
-        })
-        .map((e) => {
-          const isFirst = e.values.charAt(0) === "~";
-          const isLast = e.values.charAt(e.values.length - 1) === "~";
-          if (isFirst) {
-            e.values = e.values.substring(1);
-          }
-          if (isLast) {
-            e.values = e.values.slice(0, -1);
-          }
-          return e;
-        });
-
-      dispatch(
-        updateDatasetColumns(
-          newData,
-          dsId,
-          dfId,
-          dpId,
-          userInfo.userId,
-          isCustomSQL === "No",
-          newQuery
-        )
-      );
-      setRows([...removeExistingRowData, ...newData]);
-    }
-  };
-
   const inputFile = useRef(null);
 
   const changeHandler = () => {
@@ -302,6 +264,45 @@ export default function DSColumnTable({
     setShowViewLOVs(false);
     setIsEditLOVs(false);
     setSelectedRow(null);
+  };
+
+  const handleSaveLOV = async () => {
+    if (selectedRow.dbColumnId) {
+      const newQuery = "";
+      const removeExistingRowData = rows.filter(
+        (e) => e.uniqueId !== selectedRow.uniqueId
+      );
+      const newData = [{ ...selectedRow }]
+        .map((e) => {
+          e.values = e.values.trim();
+          return e;
+        })
+        .map((e) => {
+          const isFirst = e.values.charAt(0) === "~";
+          const isLast = e.values.charAt(e.values.length - 1) === "~";
+          if (isFirst) {
+            e.values = e.values.substring(1);
+          }
+          if (isLast) {
+            e.values = e.values.slice(0, -1);
+          }
+          return e;
+        });
+
+      dispatch(
+        updateDatasetColumns(
+          newData,
+          dsId,
+          dfId,
+          dpId,
+          userInfo.userId,
+          isCustomSQL === "No",
+          newQuery
+        )
+      );
+      setRows([...removeExistingRowData, ...newData]);
+    }
+    hideViewLOVs();
   };
 
   const LinkCell = ({ row }) => {
