9dbf422a8b9d0eebce813c485b507b58c0091368
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-04-19 22:23:28 +0530

    config_json added in status change for dataflow

--- server/src/controller/DataflowController.js
+++ server/src/controller/DataflowController.js
@@ -1200,12 +1200,12 @@ exports.activateDataFlow = async (req, res) => {
     const newVersion = parseInt($q1.rows.length ? $q1.rows[0]?.version : 1) + 1;
 
     if ($q0.rows.map((e) => e.active).includes(1)) {
-      const q2 = `UPDATE ${schemaName}.dataflow set active=1 WHERE dataflowid=$1`;
+      const q2 = `UPDATE ${schemaName}.dataflow set active=1 WHERE dataflowid=$1 returning *`;
       const q3 = `INSERT INTO ${schemaName}.dataflow_audit_log
       (dataflowid, audit_vers, audit_updt_dt, audit_updt_by, "attribute", old_val, new_val)
       VALUES($1, $2, Now(), $3, $4, $5, $6)`;
-      const q4 = `INSERT INTO ${schemaName}.dataflow_version (dataflowid, "version",  created_by, created_on)
-      VALUES($1, $2, $3, Now())`;
+      const q4 = `INSERT INTO ${schemaName}.dataflow_version (dataflowid, "version",  created_by, created_on, config_json)
+      VALUES($1, $2, $3, Now(), $4)`;
       const $q2 = await DB.executeQuery(q2, [dataFlowId]);
       const $q3 = await DB.executeQuery(q3, [
         dataFlowId,
@@ -1216,7 +1216,12 @@ exports.activateDataFlow = async (req, res) => {
         1,
       ]);
 
-      const $q4 = await DB.executeQuery(q4, [dataFlowId, newVersion, userId]);
+      const $q4 = await DB.executeQuery(q4, [
+        dataFlowId,
+        newVersion,
+        userId,
+        $q2.rows[0] || null,
+      ]);
 
       return apiResponse.successResponseWithData(res, "Operation success", {
         success: true,
@@ -1240,12 +1245,12 @@ exports.inActivateDataFlow = async (req, res) => {
     const $q0 = await DB.executeQuery(q0, [dataFlowId]);
     const newVersion = parseInt($q0.rows.length ? $q0.rows[0]?.version : 1) + 1;
 
-    const q1 = `UPDATE ${schemaName}.dataflow set active=0 WHERE dataflowid=$1`;
+    const q1 = `UPDATE ${schemaName}.dataflow set active=0 WHERE dataflowid=$1 returning *`;
     const q2 = `INSERT INTO ${schemaName}.dataflow_audit_log
     (dataflowid, audit_vers, audit_updt_dt, audit_updt_by, "attribute", old_val, new_val)
     VALUES($1, $2, Now(), $3, $4, $5, $6)`;
-    const q3 = `INSERT INTO ${schemaName}.dataflow_version (dataflowid, "version",  created_by, created_on)
-    VALUES($1, $2, $3, Now())`;
+    const q3 = `INSERT INTO ${schemaName}.dataflow_version (dataflowid, "version",  created_by, created_on, config_json)
+    VALUES($1, $2, $3, Now(), $4)`;
 
     const $q1 = await DB.executeQuery(q1, [dataFlowId]);
     const $q2 = await DB.executeQuery(q2, [
@@ -1257,7 +1262,12 @@ exports.inActivateDataFlow = async (req, res) => {
       0,
     ]);
 
-    const $q3 = await DB.executeQuery(q3, [dataFlowId, newVersion, userId]);
+    const $q3 = await DB.executeQuery(q3, [
+      dataFlowId,
+      newVersion,
+      userId,
+      $q1.rows[0] || null,
+    ]);
 
     return apiResponse.successResponseWithData(res, "Operation success", {
       success: true,
