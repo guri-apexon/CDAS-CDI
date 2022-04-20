e3080c6e5910a3338c6002429e1642376a88b9ee
Gurpreet Singh <gurpreet.singh@iqvia.com>
2022-04-20 15:39:29 +0530

    changes for logout

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -90,6 +90,25 @@ export function deleteAllCookies() {
   return true;
 }
 
+let logoutSetTimeout = null;
+const setLogoutTimeout = () => {
+  if (!logoutSetTimeout) {
+    logoutSetTimeout = setTimeout(() => {
+      console.log("Succesfully login");
+    }, 10000);
+  }
+};
+
+export function getUserId(preventRedirect) {
+  const userId = getCookie("user.id");
+  // if (preventRedirect && userId) {
+  //   setLogoutTimeout();
+  // }
+  if (!userId && !preventRedirect) {
+    window.location.reload();
+  }
+  return userId;
+}
 export function getUserInfo() {
   return {
     fullName: decodeURIComponent(`${getCookie("user.first_name")} 
@@ -98,7 +117,7 @@ export function getUserInfo() {
     lastName: getCookie("user.last_name"),
     userEmail: decodeURIComponent(getCookie("user.email")),
     lastLogin: getLastLogin(),
-    userId: getCookie("user.id"),
+    userId: getUserId(),
   };
 }
 
