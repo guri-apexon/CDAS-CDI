3174cf659246be042a3e0bd0c22e447120406d72
u1105372 <vaitheeswaran.p@iqvia.com>
2022-04-25 16:41:32 +0530

    updates for CDAS-10853

--- web/src/store/reducers/DashboardReducer.js
+++ web/src/store/reducers/DashboardReducer.js
@@ -16,6 +16,7 @@ import {
   GET_PINNED_LIST_SUCCESS,
   GET_PINNED_LIST_FAILURE,
   SELECTED_DATAFLOW,
+  UPDATE_HEADER_COUNT,
 } from "../../constants";
 
 export const initialState = {
@@ -25,16 +26,20 @@ export const initialState = {
   ingestionData: {},
   flowData: [],
   selectedCard: {
-    phase: "",
-    projectcode: "",
-    prot_id: "",
-    protocolnumber: "",
-    protocolnumberstandard: "",
-    protocolstatus: "",
-    sponsorname: "",
-    vCount: "",
-    dfCount: "",
-    dsCount: "",
+    prot_id: "ozjow62qlqtcyxc2",
+    protocolnumber: "PJIA Phase IV Study",
+    protocolnumberstandard: "PJIAPHASEIVSTUDY",
+    sponsorname: "ABBVIE",
+    phase: "Phase 4",
+    protocolstatus: "Discontinued",
+    projectcode: "XZA13449",
+    ingestionCount: "0",
+    priorityCount: "0",
+    staleFilesCount: "0",
+    dfCount: "2",
+    vCount: "1",
+    dpCount: "3",
+    dsCount: "2",
   },
   dfId: "",
   userStudies: [],
@@ -99,6 +104,21 @@ const DashboardReducer = (state = initialState, action) =>
 
       case GET_DATASET_INGESTION_SUMMARY_FAILURE:
         newState.summaryLoading = false;
+        break;
+
+      case UPDATE_HEADER_COUNT:
+        newState.selectedCard = {
+          ...state?.selectedCard,
+          dfCount: action.dfCount,
+          dsCount: action.dsCount,
+        };
+        newState.userStudies = state?.userStudies?.map((e) => {
+          if (e?.prot_id === state?.selectedCard?.prot_id) {
+            e.dfCount = action.dfCount;
+            e.dsCount = action.dsCount;
+          }
+          return e;
+        });
         break;
 
       case SELECTED_DATAFLOW:
