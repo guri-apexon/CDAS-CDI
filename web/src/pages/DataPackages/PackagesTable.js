8cfe9f8fdf0a6f1939357b070d034f32c769e5aa
Raut <niklesh.raut@iqvia.com>
2022-07-14 15:03:11 +0530

    CDAS-11096 ALM:513

--- web/src/pages/DataPackages/PackagesTable.js
+++ web/src/pages/DataPackages/PackagesTable.js
@@ -347,8 +347,25 @@ const PackagesList = ({ data, userInfo }) => {
     // }, 1000);
   };
   useEffect(() => {
-    const newData = data.packagesList || [];
-    setTableData(newData);
+    const packagesList = data.packagesList || [];
+    if (packagesList.length) {
+      const newData = packagesList.map((e) => ({
+        ...e,
+        datasets: e?.datasets?.slice()?.sort(function (a, b) {
+          if (a.mnemonic?.toUpperCase() < b.mnemonic?.toUpperCase()) {
+            return -1;
+          }
+          if (a.mnemonic?.toUpperCase() > b.mnemonic?.toUpperCase()) {
+            return 1;
+          }
+          return 0;
+        }),
+      }));
+      setTableData(newData);
+    } else {
+      setTableData(packagesList);
+    }
+
     // console.log("newData", newData);
   }, [data.packagesList]);
 
